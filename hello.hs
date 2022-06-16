8fbad969d381933da9c8d841e552f5340101aa2c
ernest404 <ernestmuisyoo@gmail.com>
2022-06-16 23:42:11 +0300

    Day 1 of Haskell

---
+++ hello.hs
@@ -0,0 +1,10 @@
+-- my first Haskell file!
+
+main = do
+    print "Hello World!"
+
+-- To compile this Use command $ ghc hello.hs
+-- If the compilation was successful, GHC will have created three files:
+-- 1. hello (hello.exe on Windows)
+-- 2. hello.hi
+-- 3. hello.o
