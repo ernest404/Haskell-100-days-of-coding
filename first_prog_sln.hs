e283fc888286c5801aedea4b1ba7d8cabd8f415f
ernest404 <ernestmuisyoo@gmail.com>
2022-06-18 00:00:00 +0300

    day 2 part 2

--- first_prog_sln.hs
+++ first_prog_sln.hs
@@ -28,3 +28,17 @@ createEmail recipient title author = toPart recipient ++ bodyPart title ++ fromP
 
 doublePlusTwo x = doubleX + 2
     where doubleX = x *2
+
+-- Write functions named inc, double, and square that increment, double, and square an argument n, respectively.
+
+inc n = n+1
+
+double n = n*2
+
+square n = n * n 
+
+-- Write a function that takes a value n. If n is even, the function returns n - 2, and if
+-- the number is odd, the function returns 3 × n + 1. To check whether the number is even,
+-- you can use either Haskell’s even function or mod (Haskell’s modulo function).
+
+oddEven n = if mod 2 n then n-2 else 3 * n +1
