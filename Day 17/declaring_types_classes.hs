d37fe6cb34ee98fd8078c30699f61591095b9825
ernest404 <ernestmuisyoo@gmail.com>
2022-07-03 23:19:47 +0300

    Day 17: types and classes

---
+++ Day 17/declaring_types_classes.hs
@@ -0,0 +1,21 @@
+-- 1. Type declaration 
+-- Introducing a new name for existing types.
+-- Importance: It makes type declarations easier to read.
+
+type String' = [Char] -- instead of list of character we call this a String
+
+hello :: String'
+hello = "Hello World"
+
+type Coordinates = (Float, Float)
+
+lamu :: Coordinates
+lamu = (2.2696, 40.9006)
+
+-- Type declarations can also be parameterized.
+type Pair a = (a, a)
+
+lamu2 :: Pair Float
+lamu2 = (2.2696, 40.9006)
+
+-- Type declarations can be nested:
